434e4093cec54b68d056b89ec31da372da69840f
shubham kumar sankhla <99600315+shubhamkumarsankhla76@users.noreply.github.com>
2025-01-22 21:30:18 +0530

    project ini

---
+++ src/main/java/com/example/Splitwise12/Splitwise12Application.java
@@ -0,0 +1,14 @@
+package com.example.Splitwise12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Splitwise12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Splitwise12Application.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/Splitwise12/Splitwise12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Splitwise12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Splitwise12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
